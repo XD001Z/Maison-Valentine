71eafc20b3c906fc51decd0b14ed1594a1e6c67e
Mortimel Ojeda <ojedamorti@gmail.com>
2023-10-01 16:53:05 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,64 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+var session = require('express-session');
+var MongoStore = require('connect-mongo');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.set('trust proxy', 1);
+app.enable('trust proxy');
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+      secure: process.env.NODE_ENV === 'production',
+      httpOnly: true,
+      maxAge: 60000
+    },
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI 
+    })
+  })
+);
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+///////////// Catch 404 and forward to handler /////////////
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+/////////////////////// Error handler ///////////////////////
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+///////////////////// MongoDB connection /////////////////////
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then((x) => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+  })
+  .catch((err) => {
+    console.error("Error connecting to Mongo: ", err);
+  });
+
+module.exports = app;
